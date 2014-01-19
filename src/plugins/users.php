36ae2c31afcab396053dde8e0c015caa98eb369c
KeitIG <keitig.dev@gmail.com>
2014-01-19 17:25:45 +0100

    update

--- src/plugins/users.php
+++ src/plugins/users.php
@@ -1,7 +1,10 @@
 <?php
-	class User {
-    	private $_pseudo;
-    	private $_password;
-    	private $_mail;
-    	private $_signup_date;
-	}
+
+class User {
+
+    private $_pseudo;
+    private $_password;
+    private $_mail;
+    private $_signup_date;
+
+}
